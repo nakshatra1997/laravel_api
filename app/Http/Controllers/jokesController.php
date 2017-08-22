10df95bbe267ad636348e93cf4a4dc6c5ee43159
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-22 19:01:33 +0100

    getting response through Response::json but returning the exact db table structure

--- app/Http/Controllers/jokesController.php
+++ app/Http/Controllers/jokesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Joke;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class jokesController extends Controller
 {
@@ -10,6 +11,22 @@ class jokesController extends Controller
     public function index()
     {
         $jokes=Joke::all(); // NOT A GOOD METHOD
-        return $jokes;
+        return Response::json(['data'=>$jokes],200);
+
+    }
+    public function show($id){
+        $joke = Joke::find($id);
+
+        if(!$joke){
+            return Response::json([
+                'error' => [
+                    'message' => 'Joke does not exist'
+                ]
+            ], 404);
+        }
+
+        return Response::json([
+            'data' => $joke
+        ], 200);
     }
 }
