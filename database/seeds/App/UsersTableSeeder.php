e4abbf1866991ae7e299a8640bf1605b62c5086c
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-22 20:29:55 +0530

    seeder classes are added in databaseSeeder.php

--- database/seeds/App/UsersTableSeeder.php
+++ database/seeds/App/UsersTableSeeder.php
@@ -2,22 +2,25 @@
 
 use Illuminate\Database\Seeder;
 use App\Joke;
+use App\User;
+
 // composer require laracasts/testdummy
 
-class Jokes extends Seeder
+
+class UsersTableSeeder extends Seeder
 {
     public function run()
     {
         // TestDummy::times(20)->create('App\Post');
         $faker = Faker\Factory::create();
 
-        foreach(range(1,30) as $index)
+        foreach(range(1,5) as $index)
         {
-            Joke->create([
-                'body' => $faker->paragraph($nbSentences = 3),
-                'user_id' =>$faker->numberBetween($min = 1, $max = 5)
+            User->create([
+                'name' => $faker->userName,
+                'email' =>$faker->email,
+                'password' =>bcrypt('secret')
             ]);
         }
     }
-
 }
